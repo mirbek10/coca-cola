ae008657940c10fce4f3034f0619d80a573f90ab
mirbek10 <m51869956@gmail.com>
2024-10-12 17:26:58 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+for (let i = 1; i<= 30; i++) {
+       console.log(i);
+       
+    if (i%3 ===0) {console.log( i ,`koka`);}
+    if (i%5 ===0) {console.log(i,`kola`);}
+    if (i%15===0) {console.log(i,`koka kola`);}
+    
+    }
+
+
+
+
+
+
+
+
+
